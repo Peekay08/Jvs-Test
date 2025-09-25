42fa2b4fa48328cfb1693738d32574ac85f27b1a
Promise <04ukiyo14@gmail.com>
2025-09-25 12:30:40 +0100

    Q1 js

---
+++ Q1.js
@@ -0,0 +1,9 @@
+function white() {
+  document.getElementById("container").style.backgroundColor = "black";
+  document.getElementById("text").style.color = "white";
+}
+
+function dark() {
+  document.getElementById("container").style.backgroundColor = "white";
+  document.getElementById("text").style.color="black";
+}

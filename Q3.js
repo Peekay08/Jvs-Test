501f6ab4244d9745909310bc9ccb929a16f7f5f8
Promise <04ukiyo14@gmail.com>
2025-09-25 12:36:48 +0100

    Q3

---
+++ Q3.js
@@ -0,0 +1,14 @@
+let x = [6, 4, 8, 3, 9, 10, 45];
+
+function sum() {
+    sum = x[0] + x[3];
+    return sum;
+}
+
+function product() {
+    product = x[0] * x[1];
+    return product;
+}
+
+console.log(sum());
+console.log(product());
